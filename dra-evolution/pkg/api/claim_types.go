67fd7a769ffa99683d1caa9a7e7517bb9b8ccba0
John Belamaric <jbelamaric@google.com>
2024-06-15 17:23:40 -0700

    Revert changes from PR 29

--- dra-evolution/pkg/api/claim_types.go
+++ dra-evolution/pkg/api/claim_types.go
@@ -258,20 +258,7 @@ type ResourceClaimStatus struct {
 
 	// ReservedFor indicates which entities are currently allowed to use
 	// the claim. A Pod which references a ResourceClaim which is not
-	// reserved for that Pod will not be started. A claim that is in
-	// use or might be in use because it has been reserved must not get
-	// deallocated.
-	//
-	// In a cluster with multiple scheduler instances, two pods might get
-	// scheduled concurrently by different schedulers. When they reference
-	// the same ResourceClaim which may only get used by one pod at a time,
-	// only one pod can be scheduled.
-	//
-	// Both schedulers try to add their pod to the claim.status.reservedFor
-	// field, but only the update that reaches the API server first gets
-	// stored. The other one fails with a conflict error and the scheduler
-	// which issued it knows that it must put the pod back into the queue,
-	// waiting for the ResourceClaim to become usable again.
+	// reserved for that Pod will not be started.
 	//
 	// There can be at most 32 such reservations. This may get increased in
 	// the future, but not reduced.
